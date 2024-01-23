a8a585c70d0bf4ec1dffcd765f6b706743bf37ea
colehearld <98860927+colehearld@users.noreply.github.com>
2024-01-23 17:54:36 +0000

    init

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("c:/Users/hearl/Downloads/terraform-project-411316-af8270881346.json")
+
+  project = "terraform-project-411316"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance2"
+  machine_type = "e2-micro"
+  zone         = "us-central1-c"
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
